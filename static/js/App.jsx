13a795e78d0c531706c1cd4e9b546de206a46c55
Erin Leeds <erin@erinleeds.com>
2020-11-20 23:07:14 +0000

    started styling

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -2,9 +2,11 @@
 function Homepage() {
 
     return (
-      <div>
+      <div >
+        <div className="title">
         <h1>#tbTweet</h1>
-        <h4>Go back in time and read your old tweets</h4>
+        <h5>Go back in time and read your old tweets</h5>
+        </div>
         <Queries/>
       </div>
     );
@@ -14,43 +16,66 @@ function Queries() {
     
     const [twitUsername, setTwitUsername] = React.useState("");
     const [numberResults, setNumberResults] = React.useState("");
-    const [minYear, setMinYear] = React.useState();
-    const [minMonth, setMinMonth] = React.useState();
-    const [minDay, setMinDay] = React.useState();
-    const [maxYear, setMaxYear] = React.useState();
-    const [maxMonth, setMaxMonth] = React.useState();
-    const [maxDay, setMaxDay] = React.useState();
+    const [minDate, setMinDate] = React.useState();
+    const [maxDate, setMaxDate] = React.useState();
     const [keyword, setKeyword]=React.useState(); //postentially add more!!
+    const [showResults, setShowResults] = React.useState(false);
 
-
-    return (    
-        <div>
+    const setResults = () => {
+        setShowResults(true);
+    }
+    return (
+        <div>  
+            <div className="queries" >
+                {/* <div>
+                    Max # results: 
+                    <input className="col s2" id="num-results" type = "text"  value = {numberResults} onChange = {e => setNumberResults(e.target.value)}></input>
+                </div> */}
+                <div className="row center">
+                <div className="col s3">
+                    <h6>Twitter username @:</h6>
+                    <input id="enter-username" type = "text"  value = {twitUsername} onChange = {e => setTwitUsername(e.target.value)}></input>
+                    
+                </div>
+                <div className="col s3" id="keyword">
+                    <h6>Search by Keyword:</h6>
+                    <input type = "text"  value = {keyword} onChange = {e => setKeyword(e.target.value)}></input>
+                </div>
+                    <div className="col s2 min-date">
+                    <h6>From:</h6>
+                        <input type = "date"  value = {minDate} onChange = {e => setMinDate(e.target.value)}></input>
+                    </div>
+                    <div className="col s2 max-date">
+                    <h6>Until:</h6>
+                        <input type = "date"  value = {maxDate} onChange = {e => setMaxDate(e.target.value)}></input>
+                        
+                    </div>
+                    <div className="col s2">
+                        <h6>Max # results: </h6>
+                        <input id="num-results" type = "text"  value = {numberResults} onChange = {e => setNumberResults(e.target.value)}></input>
+                    </div>
+                </div>
+            </div>
+            <button onClick={setResults}>Show Tweets</button>
             
-            Twitter username:
-            <input id="enter-username" type = "text"  value = {twitUsername} onChange = {e => setTwitUsername(e.target.value)}></input>
-            Max # results: 
-            <input id="num-results" type = "text"  value = {numberResults} onChange = {e => setNumberResults(e.target.value)}></input>
-            <div className="min-date">
-            From:
-                <input placeholder="year" type = "text"  value = {minYear} onChange = {e => setMinYear(e.target.value)}></input>
-                <input placeholder="month" type = "text"  value = {minMonth} onChange = {e => setMinMonth(e.target.value)}></input>
-                <input placeholder="day" type = "text"  value = {minDay} onChange = {e => setMinDay(e.target.value)}></input>
-            </div>
-            <div className="max-date">
-            Until:
-                <input placeholder="year" type = "text"  value = {maxYear} onChange = {e => setMaxYear(e.target.value)}></input>
-                <input placeholder="month" type = "text"  value = {maxMonth} onChange = {e => setMaxMonth(e.target.value)}></input>
-                <input placeholder="day" type = "text"  value = {maxDay} onChange = {e => setMaxDay(e.target.value)}></input>
-            </div>
-            <div id="keyword">
-                Search by Keyword
-                <input  type = "text"  value = {keyword} onChange = {e => setKeyword(e.target.value)}></input>
-            </div>
+            <Results showResults={showResults}/>
 
         </div>
+
     )
 
 }
 
+function Results(props) {
+    if (props.showResults) {
+        return (
+            <div>
+                results
+            </div>
+        )
+} else {
+    return <div></div>
+}
+}
 
-  ReactDOM.render(<Homepage />, document.getElementById('root'));
+ReactDOM.render(<Homepage />, document.getElementById('root'));
